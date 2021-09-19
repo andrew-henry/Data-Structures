5a3dfcd1eea83bee7e9ac351ffc33aca83a59cb5
Andrew Henry <ahjustice598@gmail.com>
2021-09-19 19:38:12 -0400

    uploaded linked list cpp file

---
+++ LinkedList.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+       int Value;
+       Node* Next;
+
+};
+
+void printList( ) {
+    
+}
+
+int main( )
+{
+      Node* head = new Node( ):
+      Node* second = new Node( ):
+      Node* third = new Node( ):
+      Node* fourth = new Node( ):
+      Node* fifth = new Node( ):
+      
+      head->Value = 1;
+      head->Next = second;
+      second->Value = 2;
+      second->Next = third;
+      third->Value = 3;
+      third->Next = fourth;
+      fourth->Value = 4;
+      fourth->Next = fifth;
+      fifth->Value = 5;
+      fifth->Next = NULL;
+      
+      system("pause>0");
+      
+
+}
+
